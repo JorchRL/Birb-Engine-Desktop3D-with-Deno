3c2dd99426a03c1e89b19f7ab46ab4ed606c6ce6
Jorge <jorch.rl@gmail.com>
2022-06-11 15:47:28 -0500

    Replace Webview with my own fork of it
    I need access to the console. But Webview_deno for some reason sets the debug flag for webview to 0.
    
    I forked the project and made the change, so for now I'm using my fork of webview_deno.

--- deps.ts
+++ deps.ts
@@ -1,4 +1,5 @@
-export {Webview} from "https://deno.land/x/webview@0.7.0-pre.1/mod.ts"
+// export {Webview} from "https://deno.land/x/webview@0.7.0-pre.1/mod.ts"
 
-export {dirname, join} from "https://deno.land/x/std/path/mod.ts"
+export { Webview } from "https://github.com/JorchRL/webview_deno/raw/main/mod.ts";
 
+export { dirname, join } from "https://deno.land/x/std/path/mod.ts";
