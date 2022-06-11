f508cecf825e42ba7bd1add1b2a1b588202aac32
Jorge <jorch.rl@gmail.com>
2022-06-11 12:35:34 -0500

    Change bundler to js

--- bundler.js
+++ bundler.js
@@ -1,11 +1,12 @@
-import {Bundler, defaultPlugins} from "https://deno.land/x/bundler/mod.ts"
+import {Bundler, defaultPlugins} from "https://github.com/JorchRL/Bundler/raw/master/mod.ts";
 
 
-const plugins = defaultPlugins()
+
+const plugins = new defaultPlugins()
+
 const bundler = new Bundler(plugins)
 
 const input = "src/index.ts"
-
 const outputMap = {[input] : "dist/app.js"}
 
 const {bundles} = await bundler.bundle([input], {outputMap})
