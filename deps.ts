21be549ef63057ea20d38d7599cd0bc190c6adff
Jorge <jorch.rl@gmail.com>
2022-06-11 13:02:49 -0500

    Clean deps.ts

--- deps.ts
+++ deps.ts
@@ -2,5 +2,3 @@ export {Webview} from "https://deno.land/x/webview@0.7.0-pre.1/mod.ts"
 
 export {dirname, join} from "https://deno.land/x/std/path/mod.ts"
 
-// @deno-types="./lib/three/types/index.d.ts"
-export * as THREE from "./lib/three/three.module.js"
