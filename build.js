30bdba86a0951822de952cc35b15349cc1e2b993
Jorge <jorch.rl@gmail.com>
2022-06-13 15:07:37 -0500

    Enable "watch mode" on build.js
    - I commented out `esbuild.stop()` so it keeps checking for changes.
    - There must be a less hacky way to achieve this though`

--- build.js
+++ build.js
@@ -1,13 +1,17 @@
-import * as esbuild from "https://deno.land/x/esbuild@v0.14.43/mod.js"
+import * as esbuild from "https://deno.land/x/esbuild@v0.14.43/mod.js";
 
 const result = await esbuild.build({
-    entryPoints: ["./src/index.ts"],
-    bundle: true,
-    outfile: "./dist/app.js",
-    write: true,
-    target: ["safari11"]
-})
+  entryPoints: ["./src/index.ts"],
+  bundle: true,
+  outfile: "./dist/app.js",
+  write: true,
+  watch: true,
+  target: ["safari11"],
+});
 
-console.log(result)
+console.log(result);
 
-esbuild.stop()
+// Disable if you want to "watch for changes!"
+// Enable for normal builds
+
+// esbuild.stop();
