daa0ccb4e0ff573f2bb4d98c1804553d2e0aed6b
Jorge <jorch.rl@gmail.com>
2022-06-15 10:26:30 -0500

    Fix app start
    - I messed things up a bit by refactoring the code at ./main.
    - Commented out the server part (as its WIP)
    - It works with `deno task dev:start` now :D

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
-const serverScript = "./server.ts";
+// const serverScript = "./server.ts";
 const clientScript = "./webview.ts";
 
 const clientCmd = ["deno", "run", "-A", "--unstable", clientScript];
-const serverCmd = ["deno", "run", serverScript];
+// const serverCmd = ["deno", "run", serverScript];
 
 const pClient = Deno.run({ cmd: clientCmd, cwd: "./" });
-const pServer = Deno.run({ cmd: serverCmd });
+// const pServer = Deno.run({ cmd: serverCmd });
 
-pServer.status();
+// pServer.status();
 
 await pClient.status();
--- webview.ts
+++ webview.ts
@@ -2,5 +2,6 @@ import { Webview } from "./deps.ts";
 
 const webview = new Webview();
 
+// Use a live-reload server. For now, until I implement it!
 webview.navigate("http://localhost:5500/dist");
 webview.run();
