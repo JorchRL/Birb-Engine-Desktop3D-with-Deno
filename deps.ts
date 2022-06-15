5da845028e97d6817e336255ac5706b2d614a961
Jorge <jorch.rl@gmail.com>
2022-06-15 10:06:36 -0500

    Add basic working server (bare server)

--- deps.ts
+++ deps.ts
@@ -1,9 +1,11 @@
 // export {Webview} from "https://deno.land/x/webview@0.7.0-pre.1/mod.ts"
 
 // I'm using my own fork of webview_deno to enable debugging on the console.
-export { Webview } from "https://github.com/JorchRL/webview_deno/raw/main/mod.ts";
+export { Webview } from "https://raw.githubusercontent.com/JorchRL/webview_deno/main/mod.ts";
 
-/// from "package.json"
+export { serve } from "https://deno.land/std@0.143.0/http/mod.ts";
+
+/// from "package.json" a reference of the version of three.js i'm bundling with this project
 
 // "dependencies": {
 //     "@types/three": "^0.141.0",
--- server.ts
+++ server.ts
@@ -1,1 +1,5 @@
 console.log("We should run a server from here...");
+
+import { serve } from "./deps.ts";
+
+serve((req) => new Response("Heloo!"), { port: 5500 });
