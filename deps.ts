660a9fb46b16843c827de0801837c66651125f63
Jorge <jorch.rl@gmail.com>
2022-06-18 11:53:47 -0500

    Add new dependencies and recache

--- deps.ts
+++ deps.ts
@@ -1,13 +1,5 @@
-// export {Webview} from "https://deno.land/x/webview@0.7.0-pre.1/mod.ts"
-
 // I'm using my own fork of webview_deno to enable debugging on the console.
 export { Webview } from "https://raw.githubusercontent.com/JorchRL/webview_deno/main/mod.ts";
 
-export { serve } from "https://deno.land/std@0.143.0/http/mod.ts";
-
-/// from "package.json" a reference of the version of three.js i'm bundling with this project
-
-// "dependencies": {
-//     "@types/three": "^0.141.0",
-//     "three": "^0.141.0"
-//   }
+export { serve } from "https://deno.land/std@0.144.0/http/mod.ts";
+export { readableStreamFromReader } from "https://deno.land/std@0.144.0/streams/mod.ts";
